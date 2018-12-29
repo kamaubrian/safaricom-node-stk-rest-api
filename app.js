e1195f103eb167ac68e5850aa6023dba7e1418fe
mtotodev05 <mtotodev05@gmail.com>
2018-12-29 09:40:40 +0300

    Setup correct database connection

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cookieParser());
 
+mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true},(err)=>{
+  if(err){
+    console.log('Error Connection To Database',err.message);
+    throw new Error(err.message);
+  }
+  console.log('MongoDB Connected Successfully');
+});
 
 
 app.use(function(req,res,next){
@@ -26,10 +35,5 @@ app.use(function(err,req,res){
     }
   })
 });
-module.exports = app;
-
-
-
-
 
 module.exports = app;
